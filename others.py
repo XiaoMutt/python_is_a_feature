6a5b2e89c6094b2a052b4bf191c55eb1fa66ceb2
Xiao Zhou <xiao.chow@gmail.com>
2021-01-20 13:40:55 -0800

    add iteration error

--- others.py
+++ others.py
@@ -1,6 +1,5 @@
 # indent using mixture of space and tab will cause problem if you are not using an IDE
 # type the following into a python console, and it will raise run time error
-
 for i in range(10):
     j = i + 1  # the indent is 4 space
     print(j)  # the indent is 1 tab
@@ -12,3 +11,20 @@ for i in range(10):
 def get_dict_of_a_to_b():
     a_to_b = dict()
     return a_to_b
+
+
+# changing iterable object while iterating lead to unexpected results
+# runtime error occur if change set or dictionary keys while iterating, but not for list
+s0 = set(range(10))
+for n in s0:
+    s0.remove(n)  # runtime error: set size change
+
+d0 = dict({i: i for i in range(10)})
+for k, v in d0.items():
+    d0.pop(k)  # runtime error
+
+l0 = list(range(10))
+for n in l0:
+    print(n)
+    l0.remove(n)  # no runtime error: lead to unexpected result
+    # if use append, then the for loop never ends
