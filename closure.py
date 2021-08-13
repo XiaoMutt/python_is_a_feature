1b022342d82dc515a52d6aa061707f061f4ebd77
Xiao Zhou <4534614+XiaoMutt@users.noreply.github.com>
2021-08-12 20:30:15 -0700

    add closure trap

---
+++ closure.py
@@ -0,0 +1,60 @@
+# Python Closure Trap:
+# when nested function access variables of an outer function and a the nested function got returned by the outer
+# function, the accessed variables will form closure in the nested function.
+# However, this does not work in the following example, when the outer function returns a list of nested functions:
+
+def this_does_not_work():
+    res = []
+    for c in "abcdefg":
+        char = c
+
+        def fun():
+            print(char)
+
+        res.append(fun)
+    return res
+
+
+funs = this_does_not_work()
+for f in funs:
+    f()
+
+
+# Is this a bug of Python?
+# In Kotlin this works perfectly fine.
+# fun outer(): ArrayList<() -> Unit> {
+#     val res = arrayListOf<() -> Unit>()
+#     for (c in arrayOf('a', 'b', 'c', 'd', 'e', 'f', 'g')) {
+#         fun nested(): Unit {
+#             println(c)
+#         }
+#         res.add(::nested)
+#     }
+#     return res
+# }
+#
+# fun main() {
+#     val funs = outer()
+#     for (f in funs) {
+#         f()
+#     }
+# }
+
+# For Python, the outer and nest functions has to be one-to-one, so the following works:
+
+def this_works():
+    res = []
+    for c in "abcdefg":
+        def generate_fun(char):
+            def fun():
+                print(char)
+
+            return fun
+
+        res.append(generate_fun(c))
+    return res
+
+
+funs = this_works()
+for f in funs:
+    f()
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Introduction
 Python is a great general purpose dynamic scripting language with English-like and literal expressions.  
 These features make programming fast. However, also due to these features, you may step into traps if you are not 
-careful. *python_is_a_feature* summarizes the traps you may encounter while using python.
+careful. *python_is_a_feature* summarizes the traps you may encounter.
 
 Python is evolving. *python_is_a_feature* uses python3.8. Some "features" may disappear in later version of python, 
 but new exiting "features" may lead to new "traps". Stay tuned!
