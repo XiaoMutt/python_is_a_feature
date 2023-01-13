8f78c73055f80cab0c66e8b5e4bbe8636db6727c
Xiao Zhou <4534614+XiaoMutt@users.noreply.github.com>
2023-01-13 13:19:47 -0800

    make updates about classes complications

--- classes_and_typing.py
+++ classes_and_typing.py
@@ -1,6 +1,12 @@
 # function and variables are treated as the same in class: they are all attributes
-# in addition, the attributes of classes are dynamic: you can add and delete at any time
-# looks like that the classes are simulated and they are more like a hashmap/dictionary object
+# in addition, classes are dynamic:
+#   - they are loaded as needed, no compiles, so circular typing will not work
+#   - you can add and delete their attribute at any time
+#   - so they are not real class which is a data type and should be static
+#   - instead, they are more like a hashmap/dictionary object simulating type and inheritance
+# the classmethod, staticmethod, property decorator use __get__, __set__ to trigger values replacement at class level
+#   - they only work on attributes defined at class level
+# lambda function cannot access class variables in class definition
 # all of these cause unexpected behavior of type checking and runtime errors
 
 class Attr(object):
@@ -9,24 +15,29 @@ class Attr(object):
 
 
 class A(object):
-    _next: A  # error, cannot do typing of a type that is has not finished defining
+    _next: A  # error, cannot do typing of a type that is has not finished defining: use "A"
 
     @staticmethod
     def helper():
         print("helper")
 
-    error = Attr(A.helper)  # error: cannot use the function in defining class either
+    error = Attr(A.helper())  # error: cannot use the function in defining class either
     good = Attr(lambda: A.helper())  # but you can use the function indirectly by putting it in a lambda
 
-    def __init__(self, next: B):  # error: B is not defined yet, you cannot use B in type
-        self._next = next
-        self.value = B.value  # but you can use B inside the functions
+    FIRST = 1
+    SECOND = FIRST + 1  # FIRST can be accessed here
+    print_first = lambda: print(FIRST)  # cannot access FIRST; no closure?
+    print_a_first = lambda: print(A.FIRST)  # should use A.FIRST
 
-        # because variable b is assigned in __init__
-        # the following will not trigger an type check error (as least in PyCharm)
-        # but will raise an run time exception
-        self.a = self.b
-        self.b = 1
+    nice_property = property(lambda self: "nice property")  # will work
+
+    def __init__(self, next_: "B"):  # error: B is not defined yet, you cannot use B in type, use "B" instead
+        self._next = next_
+        self.value = B.value  # but you can use B here, because when __init__ runs B has been defined
+        self.wrong_property = property(lambda _: "this will not work: property added in an instance")
+
+        first = 1
+        self.print_first = lambda: print(first)
 
     @property
     def next(self):
@@ -36,8 +47,20 @@ class A(object):
     def check_tuple_length(self, t: tuple):
         return len(t)
 
+    @classmethod
+    def decor(cls):
+        return lambda x: x
+
+    @decor()  # the decor property can be used as decorator in the same class definiation
+    def to_be_decorred(self):
+        pass
+
 
 class B(A):
     __slots__ = ['next']  # this will override the next property without an error thrown
     value = 1
 
+    @decor  # wrong: decor cannot be found, though B is a subclass of A
+    @A.decor()  # use A.decor
+    def to_be_decorred_too(self):
+        pass
